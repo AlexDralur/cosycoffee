6ae06aa9d120866e4ba8af6c8fb1cd9fb225a292
AlexDralur <>
2023-12-28 22:07:38 +0000

    Add basic structure to product details page and query setting for search bar

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 from .models import Product
 
 # Create your views here.
@@ -7,9 +8,27 @@ def all_products(request):
     """ This view show all products, including sorting and searches """
 
     products = Product.objects.all()
+
+    if request.GET:
+        if 's' in request.GET:
+            query = request.GET['s']
+            if not query:
+                message.error(request, 'No text was typed on the search')
+                return redirect(reverse('products'))
     
     context = {
         'products': products,
     }
 
     return render(request, 'products/products.html', context)
+
+def product_detail(request, product_id):
+    """ This view show the details of the product when user clicks on it """
+
+    product = get_object_or_404(Product, id=product_id)
+    
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
