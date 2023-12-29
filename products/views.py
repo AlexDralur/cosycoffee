2429682bfc29b19bda8fd85486b2766c0b103006
AlexDralur <>
2023-12-29 00:57:56 +0000

    Add filtering to products page!

--- products/views.py
+++ products/views.py
@@ -11,8 +11,40 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if sortkey == 'category':
+                sorkey = 'category__name'
+
+            if sortkey == 'price_ac_asc':
+                sortkey = 'price_ac'
+            elif sortkey == 'price_ac_desc':
+                sortkey = '-price_ac'
+            elif sortkey == 'price_250g_asc':
+                sortkey = 'price_250g'
+            elif sortkey == 'price_250g_desc':
+                sortkey = '-price_250g'
+            elif sortkey == 'rates_asc':
+                sortkey = 'rates'
+            elif sortkey == 'rates_desc':
+                sortkey = '-rates'
+
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if  'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -27,10 +59,15 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(brand__icontains=query)
             products = products.filter(queries)
     
+    current_sorting = f'{sort}_{direction}'
+    
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
+        'sort': sort,
+        'direction': direction,
     }
 
     return render(request, 'products/products.html', context)
