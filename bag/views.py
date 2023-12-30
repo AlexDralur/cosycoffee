d6e12a979d35b0e7605f2bf03314bf2ec59b510c
AlexDralur <>
2023-12-30 00:59:21 +0000

    Update shopping bag template

--- bag/views.py
+++ bag/views.py
@@ -19,3 +19,8 @@ def add_to_bag(request, item_id):
     
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+def clear_bag(request):
+
+    request.session['bag'] = {}
+    return redirect('view_bag')
