14fbf8d0098080954a46b9ce4ba335053a728590
AlexDralur <>
2024-01-04 00:27:41 +0000

    Attach order to profile. Update view and template

--- checkout/admin.py
+++ checkout/admin.py
@@ -12,7 +12,7 @@ class OrderAdmin(admin.ModelAdmin):
     readonly_fields = ('order_number', 'date', 
                         'delivery_cost', 'order_total','grand_total',)
 
-    fields = ('order_number', 'date', 'full_name', 
+    fields = ('order_number', 'user_profile', 'date', 'full_name', 
                 'email', 'phone_number', 'country', 'postcode', 
                 'town_or_city', 'street_address_1','street_address_2', 
                 'county', 'delivery_cost', 'order_total', 'grand_total',)
