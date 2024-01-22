2882dc2503e884820ca3c45df90874b8d683b76d
AlexDralur <>
2024-01-22 23:07:00 +0000

    Create model for producers

--- producers/models.py
+++ producers/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Producer(models.Model):
+    """Class to add producers"""
+
+    name = models.CharField(max_length=254)
+    location = models.CharField(max_length=254)
+    farm_name = models.CharField(max_length=254, null=True, blank=True)
+    altitude = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=254, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
