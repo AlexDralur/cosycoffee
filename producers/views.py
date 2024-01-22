7b71b84564554239a905dace03b847095a8616ff
AlexDralur <>
2024-01-22 23:16:17 +0000

    Create form for producers app

--- producers/views.py
+++ producers/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Producer
 
 
 def producers(request):
     """ This view returns the producers page """
+
+    model = Producer
+    
     return render(request, 'producers/producers.html')
