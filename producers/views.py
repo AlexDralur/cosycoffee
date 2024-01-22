6f9c469eba202e6cac67b8adbb57336078b5b395
AlexDralur <>
2024-01-22 23:22:48 +0000

    Add further code to provide correct view for producers page

--- producers/views.py
+++ producers/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import Producer
-
 
 def producers(request):
     """ This view returns the producers page """
 
-    model = Producer
-    
-    return render(request, 'producers/producers.html')
+    producers = Producer.objects.all()
+    return render(request, 'producers/producers.html', {'producers': producers})
