d3a19c9a51561b2df20157ac74c3d65ed8d66181
Alexander Alves <aalexanderalves@gmail.com>
2024-02-20 23:27:19 +0000

    Modify location of the 404 template

--- cosycoffee/urls.py
+++ cosycoffee/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls import handler404
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -28,3 +29,6 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('producers/', include('producers.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+handler404 = "home.views.view404"
