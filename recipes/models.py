9176be82311518ea1c42dc429a7b5b03f18fcf61
Alexander Alves <aalexanderalves@gmail.com>
2024-02-22 10:00:09 +0000

    Add model to recipe app

--- recipes/models.py
+++ recipes/models.py
@@ -2,3 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class Recipe(models.Model):
+    """Recipe Model"""
+    brewing_type = models.CharField(max_length=254, null=True, blank=True)
+    coffee_quantity = models.DecimalField(max_digits=6, decimal_places=2)
+    grinding_size = models.TextField(default='')
+    water_quantity = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=254, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
