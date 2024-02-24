def0725c0cc35c4df001eddd5a82733f69a2429b
Alexander Alves <aalexanderalves@gmail.com>
2024-02-24 20:59:03 +0000

    Fix typo in js for stripe

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,10 +1,15 @@
-/* Core logic for the stripe payments */
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
 
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
-
 var style = {
     base: {
         color: '#000',
@@ -20,31 +25,31 @@ var style = {
         iconColor: '#dc3545'
     }
 };
-var card = elements.create('card', { style: style });
+var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
-/* Logic to handle errors */
-card.addEventListener('change', function(event){
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
-    if (event.error){
+    if (event.error) {
         var html = `
-        <span>
-            <i class="fas fa-times"></i></i>
-        </span>
-        <span>${event.error.message}</span>` 
-
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
         $(errorDiv).html(html);
     } else {
         errorDiv.textContent = '';
     }
-})
+});
 
 // Handle form submit
 var form = document.getElementById('payment-form');
 
-form.addEventListener('submit', function (ev) {
+form.addEventListener('submit', function(ev) {
     ev.preventDefault();
-    card.update({ 'disabled': true });
+    card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
     $('#payment-form').fadeToggle(100);
     $('#loading-overlay').fadeToggle(100);
@@ -67,7 +72,7 @@ form.addEventListener('submit', function (ev) {
                     name: $.trim(form.full_name.value),
                     phone: $.trim(form.phone_number.value),
                     email: $.trim(form.email.value),
-                    address: {
+                    address:{
                         line1: $.trim(form.street_address_1.value),
                         line2: $.trim(form.street_address_2.value),
                         city: $.trim(form.town_or_city.value),
@@ -88,9 +93,8 @@ form.addEventListener('submit', function (ev) {
                     state: $.trim(form.county.value),
                 }
             },
-        }).then(function (result) {
+        }).then(function(result) {
             if (result.error) {
-                console.log('error in confirmCardPayment')
                 var errorDiv = document.getElementById('card-errors');
                 var html = `
                     <span class="icon" role="alert">
@@ -98,17 +102,18 @@ form.addEventListener('submit', function (ev) {
                     </span>
                     <span>${result.error.message}</span>`;
                 $(errorDiv).html(html);
-                card.update({ 'disabled': false });
+                $('#payment-form').fadeToggle(100);
+                $('#loading-overlay').fadeToggle(100);
+                card.update({'disabled': false});
                 $('#submit-button').attr('disabled', false);
             } else {
                 if (result.paymentIntent.status === 'succeeded') {
-                    console.log('paymentIntent.status === succeeded')
                     form.submit();
                 }
             }
         });
-    }).fail(function () {
+    }).fail(function() {
         // just reload the page, the error will be in django messages
         location.reload();
-    });
+    })
 });
